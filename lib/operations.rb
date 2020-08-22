9b9d6d19538baed529cccf69ce8a1c0b0f7a24c8
Ken Barlow <kenbrlow@gmail.com>
2020-08-22 22:47:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,12 +11,5 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60
-    return true
-  elsif speed < 40
-    return true
-  else
-    return false
-  end
-
+  speed > 60 ? return true : speed < 40 ? return true : return false
 end
